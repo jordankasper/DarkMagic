1114a38fba5bd01492b8ffdb3cca8a1fe8b15d2a
Jordan Kasper <jordan@gmail.com>
2014-05-06 13:16:01 -0700

    Added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :users
+  resources :sessions
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
