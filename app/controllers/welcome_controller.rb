6e4f780befd403b975662e6a9a9f30fabc335f30
Jordan Kasper <jordan@gmail.com>
2014-05-12 15:11:09 -0700

    added database query to main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
 	layout 'darkMagic'
 
+	def index
+
+		@sets = MtgSet.all
+
+		set = MtgSet.find_by_name('Worldwake')
+		@setName = set.name
+		@cardsInSet = set.cards
+
+	end
+
+
 end
